e1f5f864e61059632b0cc699048fb7fc84467197
G00397046@atu.ie <G00397046@atu.ie>
2022-12-05 16:04:29 +0000

    Created a Class called Customer

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,4 +1,39 @@
 package ie.atu;
 
 public class Person {
+    private String name;
+    private String number;
+    private String address;
+
+    public Person(){}
+
+    public Person(String name, String number, String address) {
+        this.name = name;
+        this.number = number;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
